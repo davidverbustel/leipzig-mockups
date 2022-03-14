e4a19bc3be651f53a3d0b2e0560421726cf1958c
Boris Paskhaver <boris.paskhaver@nyu.edu>
2022-03-14 11:40:27 -0400

    End of 09-07

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -28,14 +28,26 @@
             </li>
           </ul>
         </nav>
+
+        <div class="flex items-center h-full ml-auto">
+          <profile-image v-if="isLoggedIn" />
+          <action-button v-else />
+        </div>
       </div>
     </div>
   </header>
 </template>
 
 <script>
+import ActionButton from "@/components/ActionButton.vue";
+import ProfileImage from "@/components/ProfileImage.vue";
+
 export default {
   name: "MainNav",
+  components: {
+    ActionButton,
+    ProfileImage,
+  },
   data() {
     return {
       company: "Bobo Careers",
@@ -48,6 +60,7 @@ export default {
         "Students",
         "Jobs",
       ],
+      isLoggedIn: false,
     };
   },
 };
