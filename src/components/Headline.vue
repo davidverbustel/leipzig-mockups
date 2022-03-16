50f8c6f11ef13865a49827cc51c0f28b296da532
Boris Paskhaver <boris.paskhaver@nyu.edu>
2022-03-16 10:18:10 -0400

    End of 12-09

---
+++ src/components/Headline.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h1>Build for everyone</h1>
+    <h2>Find your next job at Bobo Corp</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Headline",
+  data() {
+    return {
+      sample: "Hello",
+    };
+  },
+  created() {
+    console.log(this.sample);
+  },
+};
+</script>
--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="w-full text-sm">
+  <header :class="['w-full', 'text-sm', headerHeightClass]">
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
       <div
         class="
@@ -71,6 +71,14 @@ export default {
       ],
       isLoggedIn: false,
     };
+  },
+  computed: {
+    headerHeightClass() {
+      return {
+        "h-16": !this.isLoggedIn,
+        "h-32": this.isLoggedIn,
+      };
+    },
   },
   methods: {
     loginUser() {
