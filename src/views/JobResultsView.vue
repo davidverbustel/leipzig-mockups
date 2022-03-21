7974c94e4e07782d34a3efa0a94d6ce960f2b5c4
Boris Paskhaver <boris.paskhaver@nyu.edu>
2022-03-21 17:24:13 -0400

    End of 17-02

--- src/views/JobResultsView.vue
+++ src/views/JobResultsView.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
-    <h1 class="text-6xl font-bold">Job Results View</h1>
-    <button @click="goToHome">Go to Home Page</button>
+    <job-filters-sidebar />
   </div>
 </template>
 
 <script>
+import JobFiltersSidebar from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue";
+
 export default {
   name: "JobResultsView",
-  methods: {
-    goToHome() {
-      this.$router.push({ name: "Home" });
-    },
+  components: {
+    JobFiltersSidebar,
   },
 };
 </script>
+
+    JobFiltersSidebar
