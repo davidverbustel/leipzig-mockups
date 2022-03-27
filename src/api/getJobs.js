8ac84a217f46776e3fba9d24b9146713e62bb6f8
Boris Paskhaver <boris.paskhaver@nyu.edu>
2022-03-26 21:01:02 -0400

    End of 21-04

--- src/api/getJobs.js
+++ src/api/getJobs.js
@@ -1,4 +1,9 @@
 import axios from "axios";
 
-const baseUrl = process.env.VUE_APP_API_URL;
-const response = await axios.get(`${baseUrl}/jobs`);
+const getJobs = async () => {
+  const baseUrl = process.env.VUE_APP_API_URL;
+  const response = await axios.get(`${baseUrl}/jobs`);
+  return response.data;
+};
+
+export default getJobs;
