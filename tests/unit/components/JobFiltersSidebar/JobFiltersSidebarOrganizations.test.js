257dbd9bc64ef5be91d3ea3a12b44aac9bde2502
Boris Paskhaver <boris.paskhaver@nyu.edu>
2022-03-29 17:47:46 -0400

    End of 25-07

---
+++ tests/unit/components/JobFiltersSidebar/JobFiltersSidebarOrganizations.test.js
@@ -0,0 +1,28 @@
+import { mount } from "@vue/test-utils";
+
+import JobFilterSidebarOrganizations from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue";
+
+describe("JobFiltersSidebarOrganizations", () => {
+  it("renders unique list of organizations for filtering jobs", async () => {
+    const $store = {
+      getters: {
+        UNIQUE_ORGANIZATIONS: new Set(["Google", "Amazon"]),
+      },
+    };
+    const wrapper = mount(JobFilterSidebarOrganizations, {
+      global: {
+        mocks: {
+          $store,
+        },
+        stubs: {
+          FontAwesomeIcon: true,
+        },
+      },
+    });
+    const clickableArea = wrapper.find("[data-test='clickable-area']");
+    await clickableArea.trigger("click");
+    const organizationLabels = wrapper.findAll("[data-test='organization']");
+    const organizations = organizationLabels.map((node) => node.text());
+    expect(organizations).toEqual(["Google", "Amazon"]);
+  });
+});
