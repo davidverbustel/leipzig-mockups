b67b97f7a7645457e3e3c3384f2303284e548151
Boris Paskhaver <boris.paskhaver@nyu.edu>
2022-03-30 09:25:34 -0400

    End of 25-19

--- tests/unit/components/JobFiltersSidebar/JobFiltersSidebarOrganizations.test.js
+++ tests/unit/components/JobFiltersSidebar/JobFiltersSidebarOrganizations.test.js
@@ -25,4 +25,31 @@ describe("JobFiltersSidebarOrganizations", () => {
     const organizations = organizationLabels.map((node) => node.text());
     expect(organizations).toEqual(["Google", "Amazon"]);
   });
+
+  it("communicates that user has selected checkbox for organizatin", async () => {
+    const commit = jest.fn();
+    const $store = {
+      getters: {
+        UNIQUE_ORGANIZATIONS: new Set(["Google", "Amazon"]),
+      },
+      commit,
+    };
+    const wrapper = mount(JobFilterSidebarOrganizations, {
+      global: {
+        mocks: {
+          $store,
+        },
+        stubs: {
+          FontAwesomeIcon: true,
+        },
+      },
+    });
+    const clickableArea = wrapper.find("[data-test='clickable-area']");
+    await clickableArea.trigger("click");
+    const googleInput = wrapper.find("[data-test='Google']");
+    await googleInput.setChecked();
+    expect(commit).toHaveBeenCalledWith("ADD_SELECTED_ORGANIZATIONS", [
+      "Google",
+    ]);
+  });
 });
