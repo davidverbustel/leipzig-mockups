ba7a8dad34f1749ba793c2cf46a533943f3f4040
Boris Paskhaver <boris.paskhaver@nyu.edu>
2022-03-30 09:17:24 -0400

    End of 25-18

--- tests/unit/store/index.test.js
+++ tests/unit/store/index.test.js
@@ -59,6 +59,45 @@ describe("getters", () => {
       expect(result).toEqual(new Set(["Google", "Amazon"]));
     });
   });
+
+  describe("FILTERED_JOBS_BY_ORGANIZATIONS", () => {
+    it("identifies jobs that are associated with the given organizations", () => {
+      const state = {
+        jobs: [
+          { organization: "Google" },
+          { organization: "Amazon" },
+          { organization: "Microsoft" },
+        ],
+        selectedOrganizations: ["Google", "Microsoft"],
+      };
+
+      const result = getters.FILTERED_JOBS_BY_ORGANIZATIONS(state);
+      expect(result).toEqual([
+        { organization: "Google" },
+        { organization: "Microsoft" },
+      ]);
+    });
+  });
+
+  describe("when the user has not selected any organizations", () => {
+    it("returns all jobs", () => {
+      const state = {
+        jobs: [
+          { organization: "Google" },
+          { organization: "Amazon" },
+          { organization: "Microsoft" },
+        ],
+        selectedOrganizations: [],
+      };
+
+      const result = getters.FILTERED_JOBS_BY_ORGANIZATIONS(state);
+      expect(result).toEqual([
+        { organization: "Google" },
+        { organization: "Amazon" },
+        { organization: "Microsoft" },
+      ]);
+    });
+  });
 });
 
 describe("actions", () => {
