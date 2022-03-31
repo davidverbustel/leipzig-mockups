758e3b28f04e896973c225b3bb5f750a246a4b75
Boris Paskhaver <boris.paskhaver@nyu.edu>
2022-03-31 10:44:38 -0400

    End of 26-14

--- src/store/getters.js
+++ src/store/getters.js
@@ -4,6 +4,8 @@ import {
   UNIQUE_JOB_TYPES,
   FILTERED_JOBS_BY_JOB_TYPES,
   FILTERED_JOBS,
+  INCLUDE_JOB_BY_ORGANIZATION,
+  INCLUDE_JOB_BY_JOB_TYPE,
 } from "@/store/constants";
 
 const getters = {
@@ -31,19 +33,18 @@ const getters = {
       state.selectedJobTypes.includes(job.jobType)
     );
   },
-  [FILTERED_JOBS](state) {
-    const noSelectedOrganizations = state.selectedOrganizations.length === 0;
-    const noSelectedJobTypes = state.selectedJobTypes.length === 0;
-
+  [INCLUDE_JOB_BY_ORGANIZATION]: (state) => (job) => {
+    if (state.selectedOrganizations.length === 0) return true;
+    return state.selectedOrganizations.includes(job.organization);
+  },
+  [INCLUDE_JOB_BY_JOB_TYPE]: (state) => (job) => {
+    if (state.selectedJobTypes.length === 0) return true;
+    return state.selectedJobTypes.includes(job.jobType);
+  },
+  [FILTERED_JOBS](state, getters) {
     return state.jobs
-      .filter((job) => {
-        if (noSelectedOrganizations) return true;
-        return state.selectedOrganizations.includes(job.organization);
-      })
-      .filter((job) => {
-        if (noSelectedJobTypes) return true;
-        return state.selectedJobTypes.includes(job.jobType);
-      });
+      .filter((job) => getters.INCLUDE_JOB_BY_ORGANIZATION(job))
+      .filter((job) => getters.INCLUDE_JOB_BY_JOB_TYPE(job));
   },
 };
 
--- tests/unit/store/getters.test.js
+++ tests/unit/store/getters.test.js
@@ -106,4 +106,48 @@ describe("getters", () => {
       });
     });
   });
+
+  describe("INCLUDE_JOB_BY_ORGANIZATION", () => {
+    describe("when the user has not selected any organizations", () => {
+      it("includes job", () => {
+        const state = {
+          selectedOrganizations: [],
+        };
+        const job = { organization: "Google" };
+        const includeJob = getters.INCLUDE_JOB_BY_ORGANIZATION(state)(job);
+        expect(includeJob).toBe(true);
+      });
+    });
+
+    it("identifies if job is associated with given organizations", () => {
+      const state = {
+        selectedOrganizations: ["Google", "Microsoft"],
+      };
+      const job = { organization: "Google" };
+      const includeJob = getters.INCLUDE_JOB_BY_ORGANIZATION(state)(job);
+      expect(includeJob).toBe(true);
+    });
+  });
+
+  describe("INCLUDE_JOB_BY_JOB_TYPE", () => {
+    describe("when the user has not selected any job types", () => {
+      it("includes job", () => {
+        const state = {
+          selectedJobTypes: [],
+        };
+        const job = { jobType: "Full-time" };
+        const includeJob = getters.INCLUDE_JOB_BY_JOB_TYPE(state)(job);
+        expect(includeJob).toBe(true);
+      });
+    });
+
+    it("identifies if job is associated with given organizations", () => {
+      const state = {
+        selectedJobTypes: ["Full-time", "Part-time"],
+      };
+      const job = { jobType: "Full-time" };
+      const includeJob = getters.INCLUDE_JOB_BY_JOB_TYPE(state)(job);
+      expect(includeJob).toBe(true);
+    });
+  });
 });
