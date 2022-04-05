374bde65016bdbbfdc07702b9c6d5a5508603fba
Boris Paskhaver <boris.paskhaver@nyu.edu>
2022-04-05 11:21:59 -0400

    End of 34-12

--- tests/unit/store/getters.test.ts
+++ tests/unit/store/getters.test.ts
@@ -107,19 +107,24 @@ describe("getters", () => {
   });
 
   describe("FILTERED_JOBS", () => {
-    it("filters jobs by organization and job type", () => {
+    it("filters jobs by organization, job type, and degrees", () => {
       const INCLUDE_JOB_BY_ORGANIZATION = jest.fn().mockReturnValue(true);
       const INCLUDE_JOB_BY_JOB_TYPE = jest.fn().mockReturnValue(true);
+      const INCLUDE_JOB_BY_DEGREE = jest.fn().mockReturnValue(true);
       const mockGetters = {
         INCLUDE_JOB_BY_ORGANIZATION,
         INCLUDE_JOB_BY_JOB_TYPE,
+        INCLUDE_JOB_BY_DEGREE,
       };
       const job = createJob({ title: "Best job ever" });
       const state = createState({ jobs: [job] });
+
       const result = getters.FILTERED_JOBS(state, mockGetters);
+
       expect(result).toEqual([job]);
       expect(INCLUDE_JOB_BY_ORGANIZATION).toHaveBeenCalledWith(job);
       expect(INCLUDE_JOB_BY_JOB_TYPE).toHaveBeenCalledWith(job);
+      expect(INCLUDE_JOB_BY_DEGREE).toHaveBeenCalledWith(job);
     });
   });
 });
