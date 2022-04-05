4dbac424533df9872d76491bcb80ad55a086def1
Boris Paskhaver <boris.paskhaver@nyu.edu>
2022-04-05 12:29:06 -0400

    End of 35-09

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -9,16 +9,7 @@
     "
   >
     <section class="pb-5">
-      <div class="flex flex-row justify-between">
-        <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
-        <div class="flex items-center text-sm">
-          <action-button
-            text="Clear filters"
-            type="secondary"
-            @click="clearUserJobFilterSelections"
-          />
-        </div>
-      </div>
+      <job-filters-sidebar-prompt />
 
       <accordion header="Degrees">
         <job-filters-sidebar-degrees />
@@ -37,36 +28,21 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { useStore } from "vuex";
-
-import { key } from "@/store";
 
 import Accordion from "@/components/Shared/Accordion.vue";
-import ActionButton from "@/components/Shared/ActionButton.vue";
+import JobFiltersSidebarPrompt from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarPrompt.vue";
 import JobFiltersSidebarDegrees from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarDegrees.vue";
 import JobFiltersSidebarJobTypes from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarJobTypes.vue";
 import JobFiltersSidebarOrganizations from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue";
-
-import { CLEAR_USER_JOB_FILTER_SELECTIONS } from "@/store/constants";
 
 export default defineComponent({
   name: "JobFiltersSidebar",
   components: {
     Accordion,
-    ActionButton,
+    JobFiltersSidebarPrompt,
     JobFiltersSidebarDegrees,
     JobFiltersSidebarJobTypes,
     JobFiltersSidebarOrganizations,
-  },
-  setup() {
-    const store = useStore(key);
-    const clearUserJobFilterSelections = () => {
-      store.commit(CLEAR_USER_JOB_FILTER_SELECTIONS);
-    };
-
-    return {
-      clearUserJobFilterSelections,
-    };
   },
 });
 </script>
