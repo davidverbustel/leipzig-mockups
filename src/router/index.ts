cfe8f34cf54e6ec09e92e50e1ee624f0f006b17c
David <david.verbustel@gmail.com>
2022-06-15 17:25:05 +0200

    update routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,40 +2,40 @@ import { createRouter, createWebHashHistory } from "vue-router";
 
 const HomeView = () => import("@/views/HomeView.vue");
 const JobResultsView = () =>
-  import(/* webpackChunkName: "jobs" */ "@/views/JobResultsView.vue");
+	import(/* webpackChunkName: "jobs" */ "@/views/JobResultsView.vue");
 const JobView = () =>
-  import(/* webpackChunkName: "jobs" */ "@/views/JobView.vue");
-const TeamsView = () => import("@/views/TeamsView.vue");
+	import(/* webpackChunkName: "jobs" */ "@/views/JobView.vue");
+const BlankView = () => import("@/views/BlankView.vue");
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: HomeView,
-  },
-  {
-    path: "/jobs/results",
-    name: "JobResults",
-    component: JobResultsView,
-  },
-  {
-    path: "/jobs/results/:id",
-    name: "JobListing",
-    component: JobView,
-  },
-  {
-    path: "/teams",
-    name: "Teams",
-    component: TeamsView,
-  },
+	{
+		path: "/",
+		name: "Home",
+		component: HomeView,
+	},
+	{
+		path: "/jobs/results",
+		name: "JobResults",
+		component: JobResultsView,
+	},
+	{
+		path: "/jobs/results/:id",
+		name: "JobListing",
+		component: JobView,
+	},
+	{
+		path: "/blank",
+		name: "Blank",
+		component: BlankView,
+	},
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
-  scrollBehavior() {
-    return { top: 0, left: 0, behavior: "smooth" };
-  },
+	history: createWebHashHistory(),
+	routes,
+	scrollBehavior() {
+		return { top: 0, left: 0, behavior: "smooth" };
+	},
 });
 
 export default router;
---
+++ src/views/BlankView.vue
@@ -0,0 +1,24 @@
+<template>
+	<header-container>
+		<template #title>
+			<h1 class="w-full text-4xl font-normal">Blank page</h1>
+		</template>
+
+		<template #subtitle>
+			<h2 class="w-full my-4 text-base font-light">this is a blank page</h2>
+		</template>
+	</header-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import HeaderContainer from "@/components/Shared/HeaderContainer.vue";
+
+export default defineComponent({
+	name: "TeamsView",
+	components: {
+		HeaderContainer,
+	},
+});
+</script>
